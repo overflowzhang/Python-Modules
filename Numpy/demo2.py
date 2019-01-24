2a58f4ca2c1cfb36584a487d047d926a8e975213
ZhangHe <zhanghe0309@qq.com>
2019-01-24 20:20:43 +0800

    Create demo2.py

---
+++ Numpy/demo2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+list1 = np.zeros([2,4])
+print(list,'\n')
+
+list2 = np.ones([3,5])
+print(list2,'\n')
+
+list3 = np.random.rand(2,4)
+print(list3,'\n')
+
+list4 = np.random.randint(1,10,3)
+print(list4,'\n')
+
+list5 = np.random.choice([10,20,30])
+print(list5,'\n')
